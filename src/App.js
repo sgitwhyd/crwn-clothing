b3522e7d4cf0a24b0eedb3f64e693cc433cdb28d
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-01-07 21:57:07 +0700

    updating directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory.component';
+import categories from './categories.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return <Directory categories={categories} />;
+};
 
 export default App;
