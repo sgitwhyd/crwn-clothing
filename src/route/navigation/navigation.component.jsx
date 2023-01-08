f024efa1ca130983ec849d8c281404701136abd1
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-01-08 19:53:11 +0700

    create navigation component

---
+++ src/route/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from 'react-router-dom';
+import './navigation.styles.scss';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+const Navigation = () => {
+	return (
+		<>
+			<div className='navigation'>
+				<Link to={'/'} className='logo-container'>
+					<CrwnLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link to={'/shop'} className='nav-link'>
+						SHOP
+					</Link>
+					<Link to={'/sign-in'} className='nav-link'>
+						SIGN IN
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Navigation;
